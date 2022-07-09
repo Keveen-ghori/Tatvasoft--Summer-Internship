372bec6436ffae6bffdb6689152c2450ea06b9ef
Kevin <64722488+Kjghori@users.noreply.github.com>
2022-07-09 14:27:31 +0530

    Add files via upload

---
+++ Final Project/BackEnd/BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,112 @@
+﻿using BookStore.Model.Data;
+using BookStore.Model.Model;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BookStore.Api.Controllers
+{
+
+    [Route("api/Books")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        BookRepository _bookRepository = new BookRepository();
+
+        [HttpGet]
+        [Route("list")]
+
+        public IActionResult GetBooks(int pageIndex = 1, int pageSize = 10, string keyword = "")
+        {
+            var books = _bookRepository.GetBooks(pageIndex, pageSize, keyword);
+            ListResponce<BookModel> listResponse = new ListResponce<BookModel>()
+            {
+                Results = books.Results.Select(c => new BookModel(c)),
+                TotalRecords = books.TotalRecords,
+            };
+
+            return Ok(listResponse);
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        [ProducesResponseType(typeof(BookModel), (int)HttpStatusCode.OK)]
+        public IActionResult GetCategory(int id)
+        {
+            var book = _bookRepository.GetBooks(id);
+            BookModel bookModel = new BookModel(book);
+
+            return Ok(bookModel);
+        }
+
+        [Route("add")]
+        [HttpPost]
+        [ProducesResponseType(typeof(CategoryModel), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(BadRequestObjectResult), (int)HttpStatusCode.BadRequest)]
+        public IActionResult AddBooks(BookModel book)
+        {
+            if (book == null)
+                return BadRequest("Model is null");
+
+            Book books = new Book()
+            {
+                Id = book.Id,
+                Name = book.Name,
+                Price = book.Price,
+                Description = book.Description,
+                Base64image = book.Base64image,
+                Categoryid = book.Categoryid,
+                Publisherid = book.Publisherid,
+                Quantity = book.Quantity,
+
+        };
+            var response = _bookRepository.AddBooks(books);
+            BookModel bookModel = new BookModel(response);
+
+            return Ok(bookModel);
+        }
+
+        [Route("update")]
+        [HttpPut]
+        [ProducesResponseType(typeof(CategoryModel), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(BadRequestObjectResult), (int)HttpStatusCode.BadRequest)]
+        public IActionResult UpdateBooks(BookModel book)
+        {
+            if (book == null)
+                return BadRequest("Model is null");
+
+            Book books = new Book()
+            {
+                Id = book.Id,
+                Name = book.Name,
+                Price = book.Price,
+                Description = book.Description,
+                Base64image = book.Base64image,
+                Categoryid = book.Categoryid,
+                Publisherid = book.Publisherid,
+                Quantity = book.Quantity,
+        };
+            var response = _bookRepository.UpdateBooks(books);
+            BookModel bookModel = new BookModel(response);
+
+            return Ok(bookModel);
+        }
+
+        [Route("delete/{id}")]
+        [HttpDelete]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(BadRequestObjectResult), (int)HttpStatusCode.BadRequest)]
+        public IActionResult DeleteBook(int id)
+        {
+            if (id == 0)
+                return BadRequest("id is null");
+
+            var response = _bookRepository.DeleteBooks(id);
+            return Ok(response);
+        }
+    }
+}
---
+++ Final Project/BackEnd/BookStore/BookStore/Controllers/UserController.cs
@@ -0,0 +1,139 @@
+﻿using BookStore.Model.Data;
+using BookStore.Model.Model;
+using BookStore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BookStore.Api.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        UserRepository _repository = new UserRepository();
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login(LoginModel model)
+        {
+            User user = _repository.Login(model);
+            if (user == null)
+                return NotFound();
+            return Ok(user);
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public IActionResult Register(RegisterModel model)
+        {
+            User user = _repository.Register(model);
+            if (user == null)
+                return BadRequest();
+            return Ok(user);
+        }
+
+        [HttpGet]
+        [Route("list")]
+        public IActionResult GetUsers(int pageIndex = 1, int pageSize = 10, string keyword = "")
+        {
+            ListResponce<User> response = _repository.GetUsers(pageIndex, pageSize, keyword);
+            ListResponce<UserModel> users = new ListResponce<UserModel>()
+            {
+                Results = response.Results.Select(u => new UserModel(u)),
+                ToatalRecords = response.ToatalRecords,
+            };
+
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            User user = _repository.GetUser(id);
+            if (user == null)
+                return NotFound();
+
+            UserModel userModel = new UserModel(user);
+            return Ok(userModel);
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public IActionResult UpdateUser(UserModel model)
+        {
+            try
+            {
+                if (model != null)
+                {
+                    var user = _repository.GetUser(model.Id);
+                    if (user == null)
+                        return StatusCode(HttpStatusCode.NotFound.GetHashCode(), "User not found");
+
+                    user.Firstname = model.Firstname;
+                    user.Lastname = model.Lastname;
+                    user.Email = model.Email;
+
+                    var isSaved = _repository.UpdateUser(user);
+                    if (isSaved)
+                    {
+                        return StatusCode(HttpStatusCode.OK.GetHashCode(), "User detail updated successfully");
+                    }
+                }
+
+                return StatusCode(HttpStatusCode.NotFound.GetHashCode(), "Please provide correct information");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.GetHashCode(), ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+
+        public IActionResult DeleteUser(int id)
+        {
+            try
+            {
+
+                if (id > 0)
+                {
+                    var user = _repository.GetUser(id);
+                    if (user == null)
+                    {
+                        return StatusCode(HttpStatusCode.NotFound.GetHashCode(), "user not found");
+                    }
+                    bool isDeleted = _repository.DeleteUser(user);
+                    if (isDeleted)
+                    {
+                        return StatusCode(HttpStatusCode.OK.GetHashCode(), "user deleted successfully");
+                    }
+                }
+                return StatusCode(HttpStatusCode.NotFound.GetHashCode(), "Please provide correct information");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.GetHashCode(), e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("role-list")]
+        public IActionResult GetRoles()
+        {
+            ListResponce<Role> response = _repository.GetRoles();
+            ListResponce<RoleModel> roles = new ListResponce<RoleModel>()
+            {
+                Results = response.Results.Select(u => new RoleModel(u)),
+                ToatalRecords = response.ToatalRecords,
+            };
+
+            return Ok(roles);
+        }
+    }
+}
