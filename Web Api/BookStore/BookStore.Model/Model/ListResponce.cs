aad21c63809ede7a98dc7923402ba42e989bcdf7
Kevin <64722488+Kjghori@users.noreply.github.com>
2022-07-09 14:19:48 +0530

    Add files via upload

---
+++ Web Api/BookStore/BookStore.Model/Model/ListResponce.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Model.Model
+{
+    public class ListResponce<T> where T : class
+    {
+        public int TotalRecords;
+
+        public IEnumerable<T> Results { get; set; }
+
+        public int ToatalRecords { get; set; }
+    }
+}
---
+++ Web Api/BookStore/BookStore.Model/Model/RoleModel.cs
@@ -0,0 +1,26 @@
+﻿using BookStore.Model.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Model.Model
+{
+    public class RoleModel
+    {
+        public RoleModel()
+        {
+            
+        }
+
+        public RoleModel(Role role)
+        {
+            Id = role.Id;
+            Name = role.Name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
