84e02beb42d42575067d782e5733ca77090f43a5
Kevin <64722488+Kjghori@users.noreply.github.com>
2022-07-09 14:31:57 +0530

    Add files via upload

---
+++ Final Project/BackEnd/BookStore/BookStore.Model/Model/CartModel.cs
@@ -0,0 +1,28 @@
+﻿using BookStore.Model.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Model.Model
+{
+    public class CartModel
+    {
+        public CartModel() { }
+
+        public CartModel(Cart cart)
+        {
+            Id = cart.Id;
+            UserId = cart.Userid;
+            BookId = cart.Bookid;
+            Quantity = cart.Quantity;
+            
+        }
+
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Final Project/BackEnd/BookStore/BookStore.Model/Model/PublisherModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore.Model.Data;
+
+namespace BookStore.Model.Model
+{
+    public class PublisherModel
+    {
+        public PublisherModel()
+        {
+        }
+
+        public PublisherModel(Publisher publisher)
+        {
+            Id = publisher.Id;
+            Name = publisher.Name;
+            Address = publisher.Address;
+            Contact = publisher.Contact;
+
+        }
+
+        public int Id { get; set; }
+        [Required(ErrorMessage="Publisher's Id is required")]
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+    }
+}
