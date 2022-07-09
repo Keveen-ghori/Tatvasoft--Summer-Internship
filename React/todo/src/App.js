b745eb33c6f44f86d773f003deafa83313931c54
Kevin <64722488+Kjghori@users.noreply.github.com>
2022-07-09 14:05:54 +0530

    Add files via upload

---
+++ React/todo/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <div className='cart'>
+        <h2>TITLE</h2>
+        <div className='action'>
+          <button className='btn'>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
